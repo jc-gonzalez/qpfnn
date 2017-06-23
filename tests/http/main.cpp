949e838ae592602a048e5095dedd3cde4c95cfbe
J C Gonzalez <JCGonzalez@sciops.esa.int>
2017-06-23 15:54:57 +0200

    Trying to add a test on HttpServer class

---
+++ tests/http/main.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * File:    main.cpp
+ *          This file is part of QLA Processing Framework
+ *
+ * Domain:  QPF.libQPF.main
+ *
+ * Version:  1.2
+ *
+ * Date:    2015/07/01
+ *
+ * Author:   J C Gonzalez
+ *
+ * Copyright (C) 2015,2016,2017 Euclid SOC Team @ ESAC
+ *_____________________________________________________________________________
+ *
+ * Topic: General Information
+ *
+ * Purpose:
+ *   Implement TskMng class
+ *
+ * Created by:
+ *   J C Gonzalez
+ *
+ * Status:
+ *   Prototype
+ *
+ * Dependencies:
+ *   none
+ *
+ * Files read / modified:
+ *   none
+ *
+ * History:
+ *   See <Changelog>
+ *
+ * About: License Conditions
+ *   See <License>
+ *
+ ******************************************************************************/
+
+#include <sys/time.h>
+#include "httpserver.h"
+
+int main(int argc, char * argv[])
+{
+    // Launch the HTTP Server for Docker Swarm Services, and to
+    // serve information through HTML pages. We activate the service to
+    // provide data files only if we do really need it (swarm services)
+    HttpServer * httpSrv = new HttpServer("httpSrv", "localhost:8080", true);
+
+    for (;;) {}
+
+    return 0;
+}
